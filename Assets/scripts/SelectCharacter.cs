8f97160b24367ea0cdb1ef31c3d9f196b65cdee5
RaphaelleDmitri <raphaelledmitrialabanzas@gmail.com>
2025-08-24 02:55:07 -0400

    raphaelleui

--- Assets/scripts/SelectCharacter.cs
+++ Assets/scripts/SelectCharacter.cs
@@ -1,19 +1,44 @@
 using UnityEngine;
-
-public class SelectCharacter : MonoBehaviour
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class CharacterSelectUI : MonoBehaviour
 {
-    // METHODS
-    public void PickBoy()
+    
+    public TMP_Text detailsText;     // Text to show character details
+    public Button continueButton;  // Button to go to next scene
+    public Button boyButton;
+    public Button girlButton;
+
+    private string selectedCharacter;
+
+    void Start()
     {
-        PlayerManager.Instance.SelectedCharacter = "Boy";
-        Debug.Log("Selected Boy");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+        continueButton.gameObject.SetActive(false); // Hide continue button at start
+
+        // Add listeners
+        boyButton.onClick.AddListener(OnBoyClicked);
+        girlButton.onClick.AddListener(OnGirlClicked);
+        continueButton.onClick.AddListener(OnContinueClicked);
     }
 
-    public void PickGirl()
+    void OnBoyClicked()
     {
-        PlayerManager.Instance.SelectedCharacter = "Girl";
-        Debug.Log("Selected Girl");
+        selectedCharacter = "Boy";
+        detailsText.text = "Juan Dela Cruz \n\"Tapos na po sir~\" \n\n\n\n CHARACTER DETAILS===============.";
+        continueButton.gameObject.SetActive(true); // Show Continue button
+    }
+
+    void OnGirlClicked()
+    {
+        selectedCharacter = "Girl";
+        detailsText.text = "Juania Dela Cruz \n\"Ganyan ba dapat sinasabe madaem~\" \n\n\n\n CHARACTER DETAILS===============.";
+        continueButton.gameObject.SetActive(true); // Show Continue button
+    }
+
+    void OnContinueClicked()
+    {
+        PlayerPrefs.SetString("SelectedCharacter", selectedCharacter); // Save selection
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
